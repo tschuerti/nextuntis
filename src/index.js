63f34ee11cbc2514d6a17aef610d734334faadde
DJ FLOW <czyrt2008@gmail.com>
2024-11-17 20:51:52 +0100

    Add cookie-parser and webuntis dependencies, implement cookie setting route, and update static file serving

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const { WebUntis } = require('webuntis');
 
 const app = express();
+
+app.use(cookieParser());
 
 // Template-Engine und Views-Ordner konfigurieren
 app.set('view engine', 'ejs'); // Beispiel mit EJS
@@ -12,8 +16,22 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Willkommen', message: 'Hello, Node.js!' });
 });
 
-//render media folder in /media/
-app.use('/media', express.static(path.join(__dirname, 'media')));
+// Route, um die URL-Variable `test` als Cookie zu setzen
+app.get('/set-cookie', (req, res) => {
+    const { server, school, username, password } = req.query;
+    if (!server || !school || !username || !password) {
+        return res.status(400).send('Missing connection data: Please enter server, school, username and password.');
+    } else {
+        res.cookie('server', server, { maxAge: 3600000, httpOnly: true });
+        res.cookie('school', school, { maxAge: 3600000, httpOnly: true });
+        res.cookie('username', username, { maxAge: 3600000, httpOnly: true });
+        res.cookie('password', password, { maxAge: 3600000, httpOnly: true });
+        res.send(`Connection data set: ${server}, ${school}, ${username}, ${password}`);
+    }
+});
+
+// Static-Files bereitstellen
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // Server starten
 app.listen(3000, () => {
